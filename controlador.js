5745c14b58314c70ff3a3f88a45004450753a13c
Mariana Benitez Muñoz <mariana131104@gmail.com>
2025-02-26 14:54:59 -0500

    cambio formulario

---
+++ controlador.js
@@ -0,0 +1,5 @@
+// alert("Estoy vinculado")
+
+let inputNombreUsuario,pesoUsuario=document.getElementById("pesousuario")
+let inputAlturaUsuario=document.getElementById("alturausuario")
+let botonFormulario=document.getElementById("boton")
